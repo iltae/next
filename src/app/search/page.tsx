746249e99c34ec9b818b1e499852fe81fdc9617c
김일태 <iltae94@gmail.com>
2025-02-21 19:27:57 +0900

    :sparkles: Feat: 라우팅 추가

---
+++ src/app/search/page.tsx
@@ -0,0 +1,11 @@
+// 함수형 컴퍼넌트인데 어떻게 async를 붙일 수 있냐?
+// 리액트 서버 컴퍼넌트 - 서버 측에서 사전 렌더링으로 한 번 실행하기에
+// 비동기적으로 실행해도 문제 없음
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ q: string }>;
+}) {
+  const { q } = await searchParams;
+  return <div>검색 {q}</div>;
+}
