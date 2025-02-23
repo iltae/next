61fa4b626d3cc59fd6466e4e660080d7720ae7fd
김일태 <iltae94@gmail.com>
2025-02-23 17:07:14 +0900

    :sparkles: Feat: 검색 바 로직 추가

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -1,23 +1,37 @@
 "use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation"; // next-navigation이 app router 버전 (!next-router)
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation"; // next-navigation이 app router 버전 (!next-router)
 
 export default function Searchbar() {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q"); // app router 부턴 router 객체에 query가 없음
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const onSubmit = () => {
-    router.push(`/search?q=${search}`)
-  }
+    if (!search || q === search) return; // 입력이 없거나 지금 입력이 이미 적용된 페이지라면 함수 실행 x
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
 
   return (
     <div>
-      <input value={search} onChange={onChangeSearch} />
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
       <button onClick={onSubmit}>검색</button>
     </div>
   );
