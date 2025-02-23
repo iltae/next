afb6d22f5c68f65cea02283f2ec62cef05dbca7e
김일태 <iltae94@gmail.com>
2025-02-23 17:05:22 +0900

    :sparkles: Feat: 영화 정보 타입 설정

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+export interface MovieData {
+  Title: string;
+  Year: string;
+  Rated: string;
+  Released: string;
+  Runtime: string;
+  Genre: string;
+  Director: string;
+  Writer: string;
+  Actors: string;
+  Plot: string;
+  Language: string;
+  Country: string;
+  Awards: string;
+  Poster: string;
+  Ratings: Rating[];
+  Metascore: string;
+  imdbRating: string;
+  imdbVotes: string;
+  imdbID: string;
+  Type: string;
+  DVD: string | null;
+  BoxOffice: string;
+  Production: string | null;
+  Website: string | null;
+  Response: string;
+}
+
+interface Rating {
+  Source: string;
+  Value: string;
+}
