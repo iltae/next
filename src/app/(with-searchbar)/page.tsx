cf0c7935fb17811f331171050221bde50cb0395b
김일태 <iltae94@gmail.com>
2025-02-24 17:40:21 +0900

    :sparkles: Feat: 무비 리스트 페이지 생성

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,3 +1,21 @@
+import MovieItem from "@/components/movie-item";
+import movies from "@/mock/movies.json";
+
 export default function Home() {
-  return <div>인덱스</div>;
+  return (
+    <div>
+      <section>
+        <h3>추천 영화</h3>
+        {movies.map((movie) => (
+          <MovieItem key={movie.imdbID} {...movie} />
+        ))}
+      </section>
+      <section>
+        <h3>모든 영화</h3>
+        {movies.map((movie) => (
+          <MovieItem key={movie.imdbID} {...movie} />
+        ))}
+      </section>
+    </div>
+  );
 }
--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,11 +1,21 @@
 // 함수형 컴퍼넌트인데 어떻게 async를 붙일 수 있냐?
 // 리액트 서버 컴퍼넌트 - 서버 측에서 사전 렌더링으로 한 번 실행하기에
 // 비동기적으로 실행해도 문제 없음
+
+import MovieItem from "@/components/movie-item";
+import movies from "@/mock/movies.json";
+
 export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{ q: string }>;
 }) {
   const { q } = await searchParams;
-  return <div>검색 {q}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieItem key={movie.imdbID} {...movie} />
+      ))}
+    </div>
+  );
 }
