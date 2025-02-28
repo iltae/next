437b9d94e5ef4184b85f2e8489fe47c03a914b72
김일태 <iltae94@gmail.com>
2025-02-28 11:13:53 +0900

    :sparkles: Feat: 검색바 컴퍼넌트 처리 변경

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import Searchbar from "../../components/searchbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      {/* 오직 클라이언트에서만 렌더링되게 설정 */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Searchbar />
+      </Suspense>
       {children}
     </div>
   );
