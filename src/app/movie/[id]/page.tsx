a31fc92868f75542623775ffe83c3727095ee1c2
김일태 <iltae94@gmail.com>
2025-03-07 10:26:16 +0900

    :sparkles: Feat: 요청 함수 분리 및 서버 액션 함수 생성

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -14,13 +14,7 @@ export function generateStaticParams() {
   return [{ id: "tt8370368" }, { id: "tt0371746" }];
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string | string[] }>;
-}) {
-  const { id } = await params;
-
+async function MovieDetail({ id }: { id: string }) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER}&i=${id}`);
   if (!response.ok) {
     if (response.status === 404) {
@@ -33,7 +27,7 @@ export default async function Page({
   const { Title, Released, Plot, Director, Actors, Poster } = movie;
 
   return (
-    <div>
+    <section>
       <div
         style={{ backgroundImage: `url('${Poster}')` }}
         className="before-poster relative flex justify-center bg-opacity-5 bg-cover bg-center bg-no-repeat p-[20px]"
@@ -46,8 +40,37 @@ export default async function Page({
           {Director} | {Released}
         </div>
         <div>{Actors}</div>
-        <div className="keep-all">{Plot}</div>
+        <div className="keep-all bg-gray-100 p-2">{Plot}</div>
       </div>
+    </section>
+  );
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server";
+    // 이 코드를 실행하는 서버 api를 만듬(서버 액션의 해시값을 통해 호출)
+    // 간결하고 보안성도 좋음
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+  }
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰 내용" />
+        <input name="author" placeholder="작성자" />
+        <button>작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return (
+    <div className="flex flex-col gap-2">
+      <MovieDetail id={id} />
+      <ReviewEditor />
     </div>
   );
 }
