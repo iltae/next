b54a873cf0c0166429ccafe8da559d3ce5c0bfd2
김일태 <iltae94@gmail.com>
2025-03-10 15:55:12 +0900

    :sparkles: Feat: 페이지 재검증 요청 추가

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -1,23 +1,26 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export async function createReviewAction(formData: FormData) {
   // 이 코드를 실행하는 서버 api를 만듬(서버 액션의 해시값을 통해 호출)
   // 간결하고 보안성도 좋음
-  const bookId = formData.get("bookId")?.toString();
+  const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  if (!content || !author) return;
+  if (movieId || !content || !author) return;
 
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
         method: "POST",
-        body: JSON.stringify({ bookId, content, author }),
+        body: JSON.stringify({ movieId, content, author }),
       },
     );
     console.log(response.status);
+    revalidatePath(`/movie/${movieId}`); // 페이지 재검증 요청 메서드, 서버측에서만 호출 가능, 페이지 모든 캐시 무효화, 풀라우트 캐시 무효화, 새로고침하면 다이내믹 페이지 생성
   } catch (err) {
     console.error(err);
     return;
