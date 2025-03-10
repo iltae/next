7a6270e183fb977da5d1e110d3c8ee54d2e84493
김일태 <iltae94@gmail.com>
2025-03-10 21:17:55 +0900

    :see_no_evil: Chore: 데이터 요청 캐싱 옵션 나열

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -4,6 +4,12 @@ import MovieListSkeleton from "@/components/skeleton/movie-list.skeleton";
 import { MovieData } from "@/types";
 import { Suspense } from "react";
 
+// { cache : "no-store" } - 데이터 페칭의 결과를 저장하지 않는 옵션. 즉, 캐싱을 아예 하지 않도록 하는 설정 - 기본값
+// { cache : "force-cache" } - 요청의 결과를 무조건 캐싱함. 한 번 호출된 이후에는 다시는 호출 X
+// { next : { revalidate : 3 }} - 특정 시간을 주기로 캐시 업데이트. 페이지 라우터의 ISR 방식과 유사
+// { next : { tags: ['a'] }} - On-Demand Revalidate. 요청이 들어왔을 때 데이터 최신화
+
+// Request Memoization - 중복된 API 요청 방지(하나의 페이지에서) -> 서버 컴퍼넌트 도입 때문에 생김
 // 페이지를 강제로 Dynamic 또는 Static으로 바꿀 수 있음 - 별로 권장하지 않음
 // 1. auto : 기본값, 어떠한 것도 강제하지 않음
 // 2. force-dynamic : 강제로 Dynamic 페이지로 변경
@@ -17,7 +23,6 @@ async function AllMovies() {
   });
   if (!response.ok) return <div>something went wrong...</div>;
   const allMovies: MovieData = await response.json();
-  console.log(allMovies);
   return (
     <div>
       {/* {allMovies.map((movie) => (
