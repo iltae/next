2f19bee1cf69cc026df7fb10a7b151e2ea73190e
김일태 <iltae94@gmail.com>
2025-03-10 16:24:17 +0900

    :sparkles: Feat: useActionState를 활용한 요청 로딩, 에러 처리

--- src/actions/create-review.action.ts
+++ src/actions/create-review.action.ts
@@ -2,14 +2,18 @@
 
 import { revalidatePath } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   // 이 코드를 실행하는 서버 api를 만듬(서버 액션의 해시값을 통해 호출)
   // 간결하고 보안성도 좋음
   const movieId = formData.get("movieId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  if (movieId || !content || !author) return;
+  if (movieId || !content || !author)
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
 
   try {
     const response = await fetch(
@@ -19,7 +23,9 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ movieId, content, author }),
       },
     );
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidatePath(`/movie/${movieId}`); // 페이지 재검증 요청 메서드, 서버측에서만 호출 가능, 페이지 모든 캐시 무효화, 풀라우트 캐시 무효화, 다음 요청에서 다이내믹 페이지 생성
     /*
     - 특정 경로의 모든 동적 페이지 재검증 (폴더 또는 파일의 경로 명시)
@@ -31,8 +37,15 @@ export async function createReviewAction(formData: FormData) {
     - 태그 기준, 데이터 캐시 재검증(가장 효율적)
     revalidateTag(`review-${movieId}`); -- { next : { tags : [`review-${movieId}`] }} -- 요청에 태그 설정
     */
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
+    return {
+      status: false,
+      error: "리뷰 저장에 실패했습니다.",
+    };
     return;
   }
 }
--- src/components/review-editor.tsx
+++ src/components/review-editor.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import { createReviewAction } from "@/actions/create-review.action";
+import { useActionState, useEffect } from "react";
 
 export default function ReviewEditor({ id }: { id: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+
+  useEffect(() => {
+    if (state && !state.status) alert(state.error);
+  }, [state]);
   return (
     <section>
-      <form action={createReviewAction} className="flex flex-col gap-2">
+      <form action={formAction} className="flex flex-col gap-2">
         <input name="movieId" value={id} hidden readOnly />
         <textarea
           name="content"
           placeholder="리뷰 내용"
           required
           className="box-border h-[100px] resize-y rounded-sm border p-2"
+          disabled={isPending}
         />
         <div className="flex gap-4">
           <input
@@ -17,9 +29,13 @@ export default function ReviewEditor({ id }: { id: string }) {
             placeholder="작성자"
             required
             className="box-border flex-1 rounded-sm border p-2"
+            disabled={isPending}
           />
-          <button className="w-[80px] cursor-pointer rounded-sm border-none bg-gray-50 p-2">
-            작성하기
+          <button
+            className="w-[80px] cursor-pointer rounded-sm border-none bg-gray-50 p-2"
+            disabled={isPending}
+          >
+            {isPending ? "제출 중..." : "작성하기"}
           </button>
         </div>
       </form>
